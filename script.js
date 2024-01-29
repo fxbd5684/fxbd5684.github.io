2737c2d68a3b463860b55bf90bed8d5812b00359
j-code-m <848561436@qq.com>
2024-01-29 13:07:43 +0800

    修复已知BUG 将原有的首页文字改成图片,见./imgs/ 更换首页字体 增加about界面入口
    更新用时85min

--- script.js
+++ script.js
@@ -1,34 +1,26 @@
-$(document).ready(function() {
-    $(window).scroll(function() {
-        var windowScroll = $(window).scrollTop();
+$(document).ready(function () {
+    $(window).scroll(function () {
+      var windowScroll = $(window).scrollTop();
+  
+      $(".section").each(function (index, element) {
+        var offset = $(element).offset().top;
+        var height = $(element).outerHeight();
         
-        $(".section").each(function(index, element) {
-            var offset = $(element).offset().top;
-            var height = $(element).outerHeight();
-
-            if (windowScroll >= offset && windowScroll < offset + height) {
-                if (index % 4 === 0) {
-                    $(element).css("transform", "translateX(0)");
-                } else if (index % 4 === 1) {
-                    $(element).css("transform", "translateX(0)");
-                } else if (index % 4 === 2) {
-                    $(element).css("transform", "translateX(0)");
-                } else {
-                    $(element).css("transform", "translateX(0)");
-                }
-                $(element).css("opacity", 1);
-            } else {
-                if (index % 4 === 0) {
-                    $(element).css("transform", "translateX(-50px)");
-                } else if (index % 4 === 1) {
-                    $(element).css("transform", "translateX(50px)");
-                } else if (index % 4 === 2) {
-                    $(element).css("transform", "translateX(-50px)");
-                } else {
-                    $(element).css("transform", "translateX(50px)");
-                }
-                $(element).css("opacity", 0);
-            }
-        });
+        // 在此之前，您可能需要添加一些条件来确保元素在屏幕中央时设置为 translateX(0)
+  
+        if (windowScroll >= offset && windowScroll < offset + height) {
+          if (index % 3 === 0) {
+            $(element).css("transform", "translateX(-300px)");
+          } else if (index % 3 === 1) {
+            $(element).css("transform", "translateX(300px)");
+          } else {
+            $(element).css("transform", "translateX(0)");
+          }
+          $(element).css("opacity", 1);
+        } else {
+          $(element).css("transform", "translateX(0)");
+          $(element).css("opacity", 0);
+        }
+      });
     });
-});
+  });
